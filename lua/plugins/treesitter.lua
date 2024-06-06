83679f146455f64db58f1ccc3dbf3510d38db1ed
aronyaina <admin@example.com>
2024-06-06 14:12:33 +0300

    Update of docker treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "go", "terraform", "css", "dot", "php", "python", "yaml", "json", "javascript", "rasi", "rust", "sxhkdrc", "dockerfile", "ruby" },
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "go", "terraform", "css", "dot", "php", "python", "yaml", "json", "javascript", "rasi", "rust", "sxhkdrc", "ruby", "dockerfile" },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
